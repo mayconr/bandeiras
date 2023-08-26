3d7b2eeeafea6d8019a4f14a2579da881931746f
mayconr <maycon.greg@gmail.com>
2023-08-26 13:04:35 -0300

    Project Setup

---
+++ desktop/src/org/github/bandeiras/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package org.github.bandeiras;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.github.bandeiras.BandeirasGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Bandeiras");
+		new Lwjgl3Application(new BandeirasGame(), config);
+	}
+}
